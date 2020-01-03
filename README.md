fe4f7b4d11caf1cdeb8f2e1aa56763496ced56f2
adrianteads <adrian.mederos@teads.tv>
2020-01-03 14:51:41 -0500

    update to demo cleanup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Teads React Demo
 
-This project aim to demonstrate the lifecycle of the Teads Javascript Format in a ReactJS Application
+This project aim to demonstrate the lifecycle of the Teads Javascript Format in a ReactJS Application by following the instructions on https://support.teads.tv/support/solutions/articles/36000189923-integrating-teads-formats-on-a-spa
 
 
 ### How to run the demo 
--- src/Article.js
+++ src/Article.js
@@ -1,14 +1,101 @@
-import React from 'react';
-import TeadsAd from './TeadsAd';
+import React, { Component }  from 'react';
 
-const Article = () => (
+class Article extends Component {
+  render() {
+    return (
+
   <div>
-    <h1>Article Title</h1>
+    <h1>This React project demonstrates how to Refresh ads on a SPA website</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at neque elementum velit dictum rhoncus in in felis. Sed egestas tempus diam ac interdum. Nulla lobortis massa suscipit semper ullamcorper. Cras dignissim posuere lacus id volutpat. Donec cursus aliquet laoreet. Pellentesque fermentum sagittis sem. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-    <TeadsAd  pId={84885} />
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
+    <div id="teads" />
+    <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
     <p>Quisque feugiat maximus nulla, dictum dapibus ante posuere lacinia. Nunc tempus, nisi ut convallis viverra, justo arcu cursus metus, a tristique augue orci vitae felis. Duis egestas mi id lobortis pharetra. Vestibulum non orci turpis. Nulla non velit nec magna sodales facilisis ut eu enim. Sed eget nisl at felis cursus dignissim. Aliquam eleifend maximus odio, in ultrices urna ullamcorper quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec lacinia justo vitae felis blandit scelerisque. In varius sem eu ante lobortis semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
     <p>Sed ut dui lectus. Donec dictum et purus sit amet malesuada. Maecenas interdum, libero non tempus condimentum, nulla enim ultricies lorem, quis lacinia magna risus vitae justo. Aliquam ac dolor odio. In vel risus non ligula porta ultrices et nec urna. Maecenas pharetra dolor ex, ut consectetur urna iaculis dignissim. Sed magna justo, lacinia ut nulla sit amet, finibus iaculis lorem. Duis id neque pretium, posuere purus sit amet, mattis est. Nullam sagittis ultrices velit. Phasellus venenatis sagittis mi, in finibus est aliquet id. Aliquam accumsan felis eget est vehicula, egestas malesuada leo semper.</p>
   </div>
-)
+  ); 
+  }
+
+componentDidMount() {
+
+  // For the sake of this example, here we are hard-coding an insertion (1550) to appear every time the page is refreshed. Please see the following 
+  // commended code snippet below for an example of how a publisher should implement the TAG on their wesbite.
+  setTimeout(function() {
+    (function() {
+        var placementId = 1;
+        // eslint-disable-next-line
+        var pageId = 0;
+        // eslint-disable-next-line
+        var formatName = 'inread';
+        var insertionId = 1550;
+        // eslint-disable-next-line
+        var css = 'margin: 20px auto; max-width: 500px;';
+        // eslint-disable-next-line
+        var slot = 'article';
+        // eslint-disable-next-line
+        var min = 0;
+        var pageTag = `
+          (function(w, d) {
+            try {
+              d = w.top.document || d; w = w.top.document ? w.top : w;
+            } catch (e) {}
+            var ttag = function() {
+              w.teads.page(0).placement(1, {slider: {allow_corner_position: false, allow_top_position: false}, "css":"margin: 0 auto; max-width: 550px;","format":"inread","slot":{"btf":false,"minimum":2,"slot":"p"}}).serve();
+            };
+            if (w.teads && w.teads.page) { ttag(); }
+            else if (!w.teadsscript) {
+              var s = document.createElement('script');
+              var protocol = w.location.protocol.match(/^https?:$/) ? w.location.protocol : 'https:';
+              s.src = protocol + '//a.teads.tv/media/format/v3/teads-format.min.js';
+              s.async = true; s.onload = ttag; w.teadsscript = d.getElementsByTagName('head')[0].appendChild(s);
+            } else {
+              w.teadsscript.addEventListener('load', ttag);
+            }
+          })(window, document);
+          `;
+        var script = document.createElement('script');
+        script.innerHTML = pageTag;
+        script.setAttribute('class', 'teads');
+        script.setAttribute('data-insertion-' + placementId, insertionId);
+        document.body.appendChild(script);
+    }
+    )();
+    }, 500);
+
+    // Here is the snippet which a publisher should use to load the Teads TAG. We do not use this tag for this example, because you will not always get an AD
+    // (function (d, p) {
+    //   var js, s = d.getElementsByTagName('script')[0]; js = d.createElement('script');
+    //   js.className = 'teads'; js.async = true; js.type = 'text/javascript';
+    //   js.src = '//a.teads.tv/page/'+p+'/tag';
+    //   s.parentNode.insertBefore(js, s);
+    // })(window.document, 84885);
+}
+componentWillUnmount(){
+
+  // Finally, this is the cleanup function which destroys all teads resources. It is best practice to wrap this in a try catch.
+  try{
+    window.teads.cleanup()
+  } catch(e){
+    console.log(e)
+  }
+  
+}
+  
+}
 
 export default Article; 
